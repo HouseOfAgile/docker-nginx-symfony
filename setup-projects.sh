0a03f80da9b38354ade28b7cc699f9a81128c10b
Jean-Christophe Meillaud <jc@houseofagile.com>
2018-10-11 17:13:30 +0200

    Update setup-projects.sh
    remove conf if it exist (useful in case of restart)

--- setup-projects.sh
+++ setup-projects.sh
@@ -16,6 +16,7 @@ for file in `find $SM_CONF_DIR/sm-config/ -type f -printf "%f\n" | egrep "^sm-co
 do
   source $SM_CONF_DIR/sm-config/$file
   cat /root/docker-config/default-symfony-nginx.conf | sed "s/__project_name__/$application_projectname/g;s#__project_path__#$application_install_path#g;s/__project_hosts__/$application_host/g"  > /etc/nginx/sites-available/project_$application_projectname.conf
+  [ -e /etc/nginx/sites-enabled/project_$application_projectname.conf ] && rm /etc/nginx/sites-enabled/project_$application_projectname.conf
   ln -s /etc/nginx/sites-available/project_$application_projectname.conf /etc/nginx/sites-enabled/project_$application_projectname.conf
   project_name=${file/sm-config-}
   mkdir -p $application_install_path
